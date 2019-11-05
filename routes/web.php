0b786856b8ee2682ab18e5435e68748a913a62a5
Abhijeet Kinjawadekar <abhijeet.kinjawadekar@givingforce.com>
2019-11-05 14:10:07 +0530

    updates pushed

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,21 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/society','SocietyController@index')->name('societyIndex');
-Route::get('/society/insert','SocietyController@insert')->name('societyInsert')->middleware('check.society');
+Route::get('/society/insert','SocietyController@insert')->name('societyInsert');
 Route::get('/society/edit/{soc_id}','SocietyController@edit')->name('society.edit')->middleware('check.society');
 Route::post('/society/create','SocietyController@create')->name('societyCreate');
 Route::get('/society/select/{soc_id}','SocietyController@select')->name('societySelect');
 
 Route::get('/member','MemberController@index')->name('member.index')->middleware('check.society');
-Route::get('/member/add','MemberController@add')->name('member.add')->middleware('check.society');
+Route::any('/member/add','MemberController@add')->name('member.add')->middleware('check.society');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
